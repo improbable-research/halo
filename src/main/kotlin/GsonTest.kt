23d0db45d9788a02f1b2c5688cda436c11357b7a
Daniel Tang <from_github@freshjava.co.uk>
2018-05-30 15:08:14 +0100

    added REST

---
+++ src/main/kotlin/GsonTest.kt
@@ -0,0 +1,16 @@
+import com.google.gson.Gson
+import java.io.BufferedReader
+import java.io.FileReader
+
+fun main(args : Array<String>) {
+    val gson = Gson()
+    val buff = BufferedReader(FileReader("calibrationData.json"))
+
+    val data = gson.fromJson(buff, CalibrationRawData::class.java)
+
+    for(entry in data) {
+        println(entry.key)
+        println(entry.value.servoPositions)
+        println(entry.value.data.plane.ABCD.split(",").map(String::toDouble))
+    }
+}
---
+++ src/main/kotlin/TestPayload.kt
@@ -0,0 +1,4 @@
+data class TestPayload(var name: String, var id: Int) {
+    constructor() : this("",0) {
+    }
+}
