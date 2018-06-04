2f1fb1f2e03350f72a5582498ab9133f20c6a0ec
Jon Coello <jon.coello@improbable.io>
2018-06-04 14:04:31 +0100

    Return jsons from server

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,7 @@ import io.javalin.Javalin
 import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
 
     val server = Server()
 
@@ -14,55 +14,48 @@ fun main(args : Array<String>) {
 
     val app = Javalin.create().apply {
         port(8080)
-        exception(Exception::class.java) {e, ctx -> e.printStackTrace()}
-        error(404) {ctx -> ctx.json("not found")}
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+        error(404) { ctx -> ctx.json("not found") }
     }.start()
 
     app.routes {
         get("/test/:id") { ctx ->
             response["field1"]?.id = ctx.param("id").orEmpty().toInt()
-            response["field1"]?.name = ctx.queryParam("x")?:"no value"
+            response["field1"]?.name = ctx.queryParam("x") ?: "no value"
             ctx.json(response)
 
         }
 
         post("/runModel") { ctx ->
+            // mx + c
+            val query = ctx.bodyAsClass(HelioStatNavigator.NavigationQuery::class.java)
+            ctx.status(201)
+
 
             //response in JSON
         }
 
-        post("navigate") { ctx ->
-            val payload = ctx.bodyAsClass(NavigationQueryRead::class.java)
+        post("/navigate") { ctx ->
+            val query = ctx.bodyAsClass(HelioStatNavigator.NavigationQuery::class.java)
             ctx.status(201)
 
-            val settingList = ArrayList<ServoSetting>()
-
-            for (item in payload) {
-                val navigator = HelioStatNavigator(item.params)
-                val servoSetting = navigator.computeServoSettingFromDirection(item.source, item.targetPoint)
-                settingList.add(servoSetting)
-                // Todo change above to response?/?
-            }
+            val navigator = HelioStatNavigator(query.params)
+            val servoSetting = navigator.computeServoSettingFromDirection(query.source, query.targetPoint)
+            ctx.json(servoSetting)
         }
 
-        post("navigatePointToPoint") {ctx ->
-            val payload = ctx.bodyAsClass(NavigationQueryRead::class.java)
+        post("/navigatePointToPoint") { ctx ->
+            val query = ctx.bodyAsClass(HelioStatNavigator.NavigationQuery::class.java)
             ctx.status(201)
 
-            val settingList = ArrayList<ServoSetting>()
-
-            for (item in payload) {
-                val navigator = HelioStatNavigator(item.params)
-                val servoSetting = navigator.computeServoSettingFromPoint(item.source, item.targetPoint)
-                settingList.add(servoSetting)
-                // Todo change above to response?/?
-            }
+            val navigator = HelioStatNavigator(query.params)
+            val servoSetting = navigator.computeServoSettingFromPoint(query.source, query.targetPoint)
+            ctx.json(servoSetting)
         }
 
         post("/calibrate") { ctx ->
             val payload = ctx.bodyAsClass(CalibrationDataReadAndConvert::class.java)
             ctx.status(201)
-
 
 
             var calib = HelioStatCalibration(payload)
