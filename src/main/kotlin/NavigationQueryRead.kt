dc953be5608dee9d94e5afe70a368c201607eb2e
dmculley <daveculley@improbable.io>
2018-06-04 10:25:29 +0100

    Finished first pass of query reader

--- src/main/kotlin/NavigationQueryRead.kt
+++ src/main/kotlin/NavigationQueryRead.kt
@@ -1,25 +1,47 @@
 import com.google.gson.Gson
+import io.improbable.keanu.vertices.dbl.probabilistic.GaussianVertex
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D
 import java.io.BufferedReader
 import java.io.FileReader
 
 class NavigationQueryRead(val query: HelioStatNavigator.NavigationQuery) {
 
-    fun readFromFile(filename: String) {
+    fun readFromFile(filename: String): ArrayList<HelioStatNavigator.NavigationQuery> {
         val gson = Gson()
         val buff = BufferedReader(FileReader(filename))
         val data = gson.fromJson(buff, NavigationRawQuery::class.java)
 
+        val queryList = ArrayList<HelioStatNavigator.NavigationQuery>()
+
         for(entry in data.entries) {
-            // TODO
-            // Remember to convert params to probabilistic params
+
             val params = entry.value.params.params.split(",").map(String::toDouble)
-            val pivotPoint = params[0]
-            val mPitch = params[1]
-            val cPitch = params[2]
-            val mRotation = params[3]
-            val cRotation = params[4]
+            val pivotPoint = ProbabilisticVector3D(GaussianVertex(params[0], 0.1),
+                                                   GaussianVertex(params[1], 0.1),
+                                                   GaussianVertex(params[2], 0.1))
+            val mPitch = params[3]
+            val cPitch = params[4]
+            val mRotation = params[5]
+            val cRotation = params[6]
+            val probabilisticParams = ProbabilisticHelioStatParameters(pivotPoint,
+                    ProbabilisticHelioStatParameters.ServoParameters(GaussianVertex(mPitch, 0.1),
+                                                                     GaussianVertex(cPitch, 0.1)),
+                    ProbabilisticHelioStatParameters.ServoParameters(GaussianVertex(mRotation, 0.1),
+                                                                     GaussianVertex(cRotation, 0.1)))
+
             val servoSetting = entry.value.presentControl.servoSettings.values
-//            val currentServoSetting = ServoSetting(servoSetting.first())
+            val currentServoSetting = ServoSetting(servoSetting.first(), servoSetting.last())
+
+            val sourceGeometry = entry.value.geometry.source.split(",").map(String::toDouble)
+            val source = Vector3D(sourceGeometry[0], sourceGeometry[1], sourceGeometry[2])
+
+            val targetGeometry = entry.value.geometry.targetPoint.split(",").map(String::toDouble)
+            val targetPoint = Vector3D(targetGeometry[0], targetGeometry[1], targetGeometry[2])
+
+            val query = HelioStatNavigator.NavigationQuery(probabilisticParams, currentServoSetting, source, targetPoint)
+
+            queryList.add(query)
         }
+        return queryList
     }
 }
